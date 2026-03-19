34cc13bc84a008db96c4501636b9c63ec9bf99a2
NGUYEN CHI TAN THANH <70990332+thanhnct28@users.noreply.github.com>
2026-03-19 08:28:08 +0700

    Init project + database (db trong thư mục setup)

---
+++ scr/QuanLyChiTieuCaNhan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLyChiTieuCaNhan.Global" Language="C#" %>
---
+++ scr/QuanLyChiTieuCaNhan/Site.Master.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace QuanLyChiTieuCaNhan
+{
+    public partial class SiteMaster : System.Web.UI.MasterPage
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string page = System.IO.Path.GetFileName(Request.Url.AbsolutePath);
+
+            if (Session["user"] == null && page != "DangNhap.aspx")
+            {
+                Response.Redirect("~/DangNhap.aspx");
+                return;
+            }
+
+            if (Session["name"] != null)
+            {
+                lblUser.Text = Server.HtmlEncode(Session["name"].ToString());
+            }
+
+            HighlightMenu(page);
+
+        }
+
+        void HighlightMenu(string page)
+        {
+
+            menuDashboard.Attributes.Remove("class");
+            menuChiTieu.Attributes.Remove("class");
+            menuThuNhap.Attributes.Remove("class");
+            menuDanhMuc.Attributes.Remove("class");
+            menuThongKe.Attributes.Remove("class");
+
+            switch (page)
+            {
+
+                case "Default.aspx":
+                    menuDashboard.Attributes["class"] = "active";
+                    break;
+
+                case "ChiTieu.aspx":
+                    menuChiTieu.Attributes["class"] = "active";
+                    break;
+
+                case "ThuNhap.aspx":
+                    menuThuNhap.Attributes["class"] = "active";
+                    break;
+
+                case "DanhMuc.aspx":
+                    menuDanhMuc.Attributes["class"] = "active";
+                    break;
+
+                case "ThongKe.aspx":
+                    menuThongKe.Attributes["class"] = "active";
+                    break;
+
+            }
+
+        }
+
+    }
+}
