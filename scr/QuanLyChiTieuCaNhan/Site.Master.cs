5cea030705673804d4f5b1255e1a39a73dc4455d
NGUYEN CHI TAN THANH <70990332+thanhnct28@users.noreply.github.com>
2026-03-22 10:31:08 +0700

    Add files via upload

--- scr/QuanLyChiTieuCaNhan/Site.Master.cs
+++ scr/QuanLyChiTieuCaNhan/Site.Master.cs
@@ -4,39 +4,37 @@ namespace QuanLyChiTieuCaNhan
 {
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
-
         protected void Page_Load(object sender, EventArgs e)
         {
-
             string page = System.IO.Path.GetFileName(Request.Url.AbsolutePath);
 
+            // Check login
             if (Session["user"] == null && page != "DangNhap.aspx")
             {
                 Response.Redirect("~/DangNhap.aspx");
                 return;
             }
 
+            // Hiển thị tên user
             if (Session["name"] != null)
             {
                 lblUser.Text = Server.HtmlEncode(Session["name"].ToString());
             }
 
             HighlightMenu(page);
-
         }
 
         void HighlightMenu(string page)
         {
-
-            menuDashboard.Attributes.Remove("class");
-            menuChiTieu.Attributes.Remove("class");
-            menuThuNhap.Attributes.Remove("class");
-            menuDanhMuc.Attributes.Remove("class");
-            menuThongKe.Attributes.Remove("class");
+            // Reset class
+            menuDashboard.Attributes["class"] = "";
+            menuChiTieu.Attributes["class"] = "";
+            menuThuNhap.Attributes["class"] = "";
+            menuDanhMuc.Attributes["class"] = "";
+            menuThongKe.Attributes["class"] = "";
 
             switch (page)
             {
-
                 case "Default.aspx":
                     menuDashboard.Attributes["class"] = "active";
                     break;
@@ -56,10 +54,7 @@ namespace QuanLyChiTieuCaNhan
                 case "ThongKe.aspx":
                     menuThongKe.Attributes["class"] = "active";
                     break;
-
             }
-
         }
-
     }
 }
